52561ce507caaa8b35d1d0ccb4f789f9b893bb08
Ville Stenström <villestenstrom@gmail.com>
2023-09-19 19:29:14 +0200

    working kattis reworked

--- labb2/ClosestWords.java
+++ labb2/ClosestWords.java
@@ -8,58 +8,53 @@ import java.util.List;
 public class ClosestWords {
   LinkedList<String> closestWords = null;
   int closestDistance = -1;
-  char[] latestWord = {' '};
+  String latestWord = "";
   int longestWord = 40;
-  int[][] memo = new int[40][40];
+  int[][] memo = new int[longestWord][longestWord];
 
-  int partDist(char[] w1, char[] w2, int w1len, int w2len, int start) {
+  int partDist(String w1, String w2, int w1len, int w2len, int start) {
 
     for (int i = 0; i <= w1len; i++) {
-      memo[i][0] = i;
-    }
-    for (int j = 0; j <= w2len; j++) {
-      memo[0][j] = j;
-    }
-
-    start = Math.max(1, start);
-    for (int i = 1; i <= w1len; i++) {
       for (int j = start; j <= w2len; j++) {
-        int cost = w1[i - 1] == w2[j - 1] ? 0 : 1;
-        int addLetter = memo[i - 1][j] + 1;
-        int deleteLetter = memo[i][j - 1] + 1;
-        int changeLetter = memo[i - 1][j - 1] + cost;
-        memo[i][j] = Math.min(Math.min(addLetter, deleteLetter),
-            changeLetter);
+        if (i == 0) {
+          memo[i][j] = j;
+        } else if (j == 0) {
+          memo[i][j] = i;
+        } else {
+          int addLetter = memo[i - 1][j] + 1;
+          int deleteLetter = memo[i][j - 1] + 1;
+          int replaceCost = (w1.charAt(i - 1) == w2.charAt(j - 1)) ? 0 : 1;
+          int replaceLetter = memo[i - 1][j - 1] + replaceCost;
+
+          memo[i][j] = Math.min(Math.min(addLetter, deleteLetter), replaceLetter);
+        }
       }
     }
     return memo[w1len][w2len];
   }
 
   int distance(String w1, String w2, int start) {
-    return partDist(w1.toCharArray(), w2.toCharArray(), w1.length(), w2.length(), start);
+    return partDist(w1, w2, w1.length(), w2.length(), start);
   }
 
   public ClosestWords(String w, List<String> wordList) {
     int differentLettersIndex = 0;
 
     for (String s : wordList) {
-      char[] sChar = s.toCharArray();
       if (closestDistance != -1) {
         int lengthDifference = Math.abs(s.length() - w.length());
         if (lengthDifference > closestDistance) {
           continue;
         }
-
-        differentLettersIndex = 1;
-        for (int i = 0; i < Math.min(sChar.length, latestWord.length); i++) {
-          if (sChar[i] != latestWord[i]) {
+        differentLettersIndex = 0;
+        for (int i = 0; i < Math.min(s.length(), latestWord.length()); i++) {
+          if (s.charAt(i) != latestWord.charAt(i)) {
             break;
           }
           differentLettersIndex++;
         }
-
       }
-      latestWord = sChar;
+      latestWord = s;
 
       int dist = distance(w, s, differentLettersIndex);
       // System.out.println("d(" + w + "," + s + ")=" + dist);
