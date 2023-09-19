fe4c5974220988937f66c89d6e9847d57e7122d2
Ville Stenström <villestenstrom@gmail.com>
2023-09-19 13:15:56 +0200

    add files for every lab

--- labb2/ClosestWords.java
+++ labb2/ClosestWords.java
@@ -8,9 +8,9 @@ import java.util.List;
 public class ClosestWords {
   LinkedList<String> closestWords = null;
   int closestDistance = -1;
-  String latestWord = "";
+  char[] latestWord = { ' ' };
 
-  int partDist(String w1, String w2, int w1len, int w2len, int[][] memo) {
+  int partDist(char[] w1, char[] w2, int w1len, int w2len, int[][] memo) {
 
     int memo_value = memo[w1len][w2len];
     if (memo_value != 0)
@@ -21,55 +21,56 @@ public class ClosestWords {
     if (w2len == 0)
       return w1len;
 
-    int addLetter = partDist(w1, w2, w1len - 1, w2len, memo) + 1;
-    int deleteLetter = partDist(w1, w2, w1len, w2len - 1, memo) + 1;
     int replaceLetter = partDist(w1, w2, w1len - 1, w2len - 1, memo) +
-        (w1.charAt(w1len - 1) == w2.charAt(w2len - 1) ? 0 : 1);
-  
+        (w1[w1len - 1] == w2[w2len - 1] ? 0 : 1);
+    int deleteLetter = partDist(w1, w2, w1len, w2len - 1, memo) + 1;
+    int addLetter = partDist(w1, w2, w1len - 1, w2len, memo) + 1;
+
     int res = Math.min(Math.min(addLetter, deleteLetter), replaceLetter);
+
     memo[w1len][w2len] = res;
     return res;
   }
 
-  int distance(String w1, String w2, int[][] memo) {
-    return partDist(w1, w2, w1.length(), w2.length(), memo);
+  int distance(char[] w1, char[] w2, int[][] memo) {
+    return partDist(w1, w2, w1.length, w2.length, memo);
   }
 
   public ClosestWords(String w, List<String> wordList) {
     int longestWord = 35;
+    int[] zeroArray = new int[longestWord];
     int[][] memo = new int[w.length() + 1][longestWord];
     int differentLettersIndex = 0;
-    closestWords = new LinkedList<String>();
     for (String s : wordList) {
+      char[] sCharArray = s.toCharArray();
       if (closestDistance != -1) {
         int lengthDifference = Math.abs(s.length() - w.length());
         if (lengthDifference > closestDistance) {
           continue;
         }
-        
 
         differentLettersIndex = 0;
-        for (int i = 0; i < Math.min(s.length(), latestWord.length()); i++) {
-          if (s.charAt(i) != latestWord.charAt(i)) {
+        for (int i = 0; i < Math.min(s.length(), latestWord.length); i++) {
+          if (sCharArray[i] != latestWord[i]) {
             break;
           }
           differentLettersIndex++;
         }
 
       }
-      latestWord = s;
+      latestWord = sCharArray;
 
       for (int i = 0; i < w.length() + 1; i++) {
-        for (int j = differentLettersIndex; j < longestWord; j++) {
-          memo[i][j] = 0;
-        }
+        System.arraycopy(zeroArray, 0, memo[i], differentLettersIndex, longestWord - differentLettersIndex);
       }
 
-      int dist = distance(w, s, memo);
+      char[] wCharArray = w.toCharArray();
+
+      int dist = distance(wCharArray, sCharArray, memo);
       // System.out.println("d(" + w + "," + s + ")=" + dist);
       if (dist < closestDistance || closestDistance == -1) {
         closestDistance = dist;
-        closestWords.clear();
+        closestWords = new LinkedList<String>();
         closestWords.add(s);
       } else if (dist == closestDistance)
         closestWords.add(s);
